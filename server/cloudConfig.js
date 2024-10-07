08987574e18c2a75641ebef8deb8502e66e4c476
Omkar Gadam <omkargangadhargadam@gmail.com>
2024-10-08 02:14:13 +0530

    changed cloudniary

--- server/cloudConfig.js
+++ server/cloudConfig.js
@@ -1,22 +1,24 @@
-
 import dotenv from 'dotenv';
 dotenv.config();
-import { v2 as cloudinary } from 'cloudinary';
+
+import cloudinary from 'cloudinary';
+const { v2: cloudinaryV2 } = cloudinary;
+
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
 
-cloudinary.config({
+// Use cloudinaryV2 for configuration
+cloudinaryV2.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.CLOUD_API_KEY,
     api_secret: process.env.CLOUD_API_SECRET
 });
 
-
 const storage = new CloudinaryStorage({
-    cloudinary: cloudinary,
+    cloudinary: cloudinaryV2, // Use cloudinaryV2 here
     params: {
         folder: 'twitter_DEV',
-        allowedFormats: ["png", "jpg", "jpeg"],
+        allowed_formats: ["png", "jpg", "jpeg"], // This property is lowercase
     },
 });
 
-export { cloudinary, storage };
+export { cloudinaryV2 as cloudinary, storage }; // Export cloudinaryV2 as cloudinary
